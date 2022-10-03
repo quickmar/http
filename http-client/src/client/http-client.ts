45b68c0955a5ae8b0a64295fabfa29ec53bdb347
Marek Mialkowski <marekmial15@gmail.com>
2022-10-03 14:18:02 +0200

    refactor http client class

--- http-client/src/client/http-client.ts
+++ http-client/src/client/http-client.ts
@@ -1,59 +1,7 @@
 import { RequestBuilder } from "../request-builder/request-builder";
+import { AbstractHttpClient } from "./http-client.abstract";
 
-export abstract class HttpClient<T extends RequestBuilder> {
-  #baseUrl: T;
-  #baseSearchParam: URLSearchParams;
-  #paths: Map<string, () => T> = new Map();
-
-  constructor(baseUrl: T) {
-    this.#baseUrl = baseUrl;
-    this.#baseSearchParam = new URL("", this.#baseUrl.toString()).searchParams;
-  }
-
-  public hasPath(path: string): boolean {
-    return this.#paths.has(path)
-  }
-
-  public registerPath(path: string, builderFn?: (baseBuilder: T) => T): void {
-    const httpClientBuilder = this.#baseUrl.clone(path);
-    this.#baseSearchParam.forEach((value, key) =>
-      httpClientBuilder.addSearchParam(key, value)
-    );
-    if (builderFn) {
-      const userBuilder = builderFn(httpClientBuilder);
-      if (httpClientBuilder !== userBuilder) {
-        throw new ReferenceError(
-          "Can not register path. Your builder is not the same as given baseBuilder."
-        );
-      }
-      const copy = userBuilder.clone();
-      this.#paths.set(path, () => copy.clone());
-      return;
-    }
-    this.#paths.set(path, () => httpClientBuilder.clone());
-  }
-
-  public getBuilder(path: string): T {
-    const supplierFn = this.#paths.get(path);
-    if (supplierFn) return supplierFn();
-    throw new Error(`Path "${path}" could not be found!`);
-  }
-
-  public getRequest(path: string): Request {
-    return this.getBuilder(path).build();
-  }
-
-  public fetch(path: string, body?: BodyInit): Promise<Response> {
-    if (body) return this.getBuilder(path).addBody(body).fetch();
-    return this.getBuilder(path).fetch();
-  }
-
-  get url(): string {
-    return this.#baseUrl.toString();
-  }
-}
-
-export class HttpClientBase extends HttpClient<RequestBuilder> {
+export class HttpClient extends AbstractHttpClient<RequestBuilder> {
   constructor(baseUrl: RequestBuilder | string) {
     if (baseUrl instanceof RequestBuilder) {
       super(baseUrl);
--- http-client/src/main.ts
+++ http-client/src/main.ts
@@ -1,9 +1,9 @@
-import { HttpClientBase } from "./client/http-client";
+import { HttpClient } from "./client/http-client";
 import { RequestBuilder } from "./request-builder/request-builder";
 
 export class Http {
-  static createClient(baseUrl: string | RequestBuilder): HttpClientBase {
-    return new HttpClientBase(baseUrl);
+  static createClient(baseUrl: string | RequestBuilder): HttpClient {
+    return new HttpClient(baseUrl);
   }
 }
 const client = Http.createClient(
@@ -17,8 +17,3 @@ client.registerPath("test");
 console.dir(client.getBuilder("test").toString());
 client.registerPath("test2", (b) => b.addDistinctSearchParam("test2", "param"));
 console.dir(client.getBuilder("test2").toString());
-// client.registerPath('test3', (b) => b.clone().addDistinctSearchParam('throw', 'error'))
-client
-  .fetch("test2")
-  .then((res) => res.text())
-  .then(console.log);
