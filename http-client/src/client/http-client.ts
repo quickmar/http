c543ab047bc9f93ac229cf2e32b42de7f1b85917
Marek Mialkowski <marekmial15@gmail.com>
2022-10-03 14:14:53 +0200

    Change forrmating

--- http-client/src/client/http-client.ts
+++ http-client/src/client/http-client.ts
@@ -6,35 +6,43 @@ export abstract class HttpClient<T extends RequestBuilder> {
   #paths: Map<string, () => T> = new Map();
 
   constructor(baseUrl: T) {
-    this.#baseUrl = baseUrl; 
-    this.#baseSearchParam = new URL('', this.#baseUrl.toString()).searchParams;
+    this.#baseUrl = baseUrl;
+    this.#baseSearchParam = new URL("", this.#baseUrl.toString()).searchParams;
   }
 
-  public registerPath(path: string, builderFn?: (baseBuilder:T) => T): void {
+  public hasPath(path: string): boolean {
+    return this.#paths.has(path)
+  }
+
+  public registerPath(path: string, builderFn?: (baseBuilder: T) => T): void {
     const httpClientBuilder = this.#baseUrl.clone(path);
-    this.#baseSearchParam.forEach((value, key) => httpClientBuilder.addSearchParam(key, value))
+    this.#baseSearchParam.forEach((value, key) =>
+      httpClientBuilder.addSearchParam(key, value)
+    );
     if (builderFn) {
-      const userBuilder = builderFn(httpClientBuilder)
+      const userBuilder = builderFn(httpClientBuilder);
       if (httpClientBuilder !== userBuilder) {
-        throw new ReferenceError('Can not register path. Your builder is not the same as given baseBuilder.')
+        throw new ReferenceError(
+          "Can not register path. Your builder is not the same as given baseBuilder."
+        );
       }
-      const copy = userBuilder.clone()
-      this.#paths.set(path, () => copy.clone())
+      const copy = userBuilder.clone();
+      this.#paths.set(path, () => copy.clone());
       return;
     }
-    this.#paths.set(path, () => httpClientBuilder.clone())
+    this.#paths.set(path, () => httpClientBuilder.clone());
   }
 
-  public  getBuilder(path: string): T {
+  public getBuilder(path: string): T {
     const supplierFn = this.#paths.get(path);
     if (supplierFn) return supplierFn();
     throw new Error(`Path "${path}" could not be found!`);
   }
 
   public getRequest(path: string): Request {
-    return this.getBuilder(path).build()
+    return this.getBuilder(path).build();
   }
-  
+
   public fetch(path: string, body?: BodyInit): Promise<Response> {
     if (body) return this.getBuilder(path).addBody(body).fetch();
     return this.getBuilder(path).fetch();
@@ -50,7 +58,7 @@ export class HttpClientBase extends HttpClient<RequestBuilder> {
     if (baseUrl instanceof RequestBuilder) {
       super(baseUrl);
     } else {
-      super(RequestBuilder.create(baseUrl))
+      super(RequestBuilder.create(baseUrl));
     }
   }
 }
